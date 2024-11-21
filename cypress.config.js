7813326a826b92fe53472ec4529b1dca41560d0e
dashatolstaya <dasha.tolstaya18@gmail.com>
2024-11-21 16:35:43 +0200

    Add button visability tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'site path',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
     viewportWidth: 1280,
