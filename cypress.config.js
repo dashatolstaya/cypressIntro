227397941ca374280f6bb435cfa3f55b9d46f9b1
dashatolstaya <dasha.tolstaya18@gmail.com>
2024-12-30 17:17:15 +0200

    Add gitActions workflow

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress")
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'false',
+    supportFile: 'cypress/support/e2e.js',
     viewportWidth: 1280,
     viewportHeight: 720,
     retries: {
